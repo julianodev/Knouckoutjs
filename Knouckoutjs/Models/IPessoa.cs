9d22cfc927bfdef4aeee1912d6e875fb8c05b7e0
Lucas juliano <lucas.juliano@live.com>
2017-10-05 18:53:10 -0300

    ajustes iniciais

---
+++ Knouckoutjs/Models/IPessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knouckoutjs.Models
+{
+    public interface IPessoa
+    {
+        string Nome { get; set; }
+        string Sobrenome { get; set; }
+
+        DateTime Idade { get; set;}
+
+        string Email { get; set; }
+
+        IEnumerable<Pessoa> Get();
+
+        Pessoa GetById(long id);
+
+        bool Create (Pessoa pessoa);
+
+        bool Update(Pessoa pessoa);
+
+        bool Delete (Pessoa pessoa);
+
+    }
+}
---
+++ Knouckoutjs/Models/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Knouckoutjs.Models
+{
+    public class Pessoa : IPessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime Idade { get; set; }
+        public string Email { get; set; }
+
+
+        public IEnumerable<Pessoa> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pessoa GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(Pessoa pessoa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Pessoa pessoa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Pessoa pessoa)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
